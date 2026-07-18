56b96349305b5a3a4d4cab22354982ad3d1e832b
Ertugrul998 <ertugrulpisgin15@gmail.com>
2026-07-18 17:10:12 +0300

    Add files via upload

---
+++ 24.1_kullanici_kayit_ve_giris_sistemi.py
@@ -0,0 +1,42 @@
+# Proje 24 - Kullanıcı Kayıt ve Giriş Sistemi
+
+kullanici_adi = input("Kullanıcı adı giriniz: ")
+sifre = input("Şifre giriniz: ")
+
+kullanici_var = False
+
+with open("kullanicilar.txt", "r", encoding="utf-8") as dosya:
+    for satir in dosya:
+        kayitli_ad, kayitli_sifre = satir.strip().split(",")
+
+        if kullanici_adi == kayitli_ad:
+            kullanici_var = True
+            break
+
+if kullanici_var:
+    print("Bu kullanıcı adı zaten kayıtlı!")
+else:
+    with open("kullanicilar.txt", "a", encoding="utf-8") as dosya:
+        dosya.write(kullanici_adi + "," + sifre + "\n")
+
+    print("Kullanıcı kaydedildi!")
+
+giris_adi = input("Giriş için kullanıcı adını giriniz: ")
+giris_sifre = input("Giriş için şifrenizi giriniz: ")
+
+giris_basarili = False
+
+with open("kullanicilar.txt", "r", encoding="utf-8") as dosya:
+    for satir in dosya:
+        kayitli_ad, kayitli_sifre = satir.strip().split(",")
+
+        if giris_adi == kayitli_ad and giris_sifre == kayitli_sifre:
+            giris_basarili = True
+            break
+
+if giris_basarili:
+    print("\n--- KULLANICI PANELİ ---")
+    print("Hoş geldiniz,", giris_adi)
+    print("Sistem kullanıma hazır.")
+else:
+    print("Kullanıcı adı veya şifre hatalı.")
